0e211f18ef8883835178ce1a0c09c6027ed290fb
Christopher-VA <cvonaltenstadt@gmail.com>
2023-12-11 21:09:29 +0800

    Initail start, setting up the code

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require('./utils/generateMarkdown')
+
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+}];
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (error) => console.log(error))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+    .then((answers) => {
+        writeToFile('README.md', answers);
+    })
+}
 
 // Function call to initialize app
 init();
